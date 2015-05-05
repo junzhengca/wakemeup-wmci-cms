7ae28c57c1a1fdd3d7cd0ddb4c1cbe51180b22a6
Jun Zheng <furaiguburu@gmail.com>
2015-05-05 00:08:45 -0600

    Welcome screen

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WAKE ME UP - a Walter Murray Symposium</title>
+        <link href="style.css" type="text/css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    	<div id="splash_container">
+        	<div id="school_img"></div>
+            <div id="bottom_mask"></div>
+            <div id="wordcloud"></div>
+            <div id="wakemeup_logo"></div>
+            <div id="subtitle_img"></div>
+            <div id="location_img"></div>
+            <div id="enter_button"></div>
+        </div>
+    </body>
+</html>
